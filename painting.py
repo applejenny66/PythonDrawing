53ef9e8cf00d2b5084f4869f3450c47ed3dea4ce
applejenny66 <applejenny66@gmail.com>
2019-09-28 21:59:12 +0800

    .

--- painting.py
+++ painting.py
@@ -8,87 +8,79 @@ from preprocess import Kmeans
 from monitor import Monitor
 import tkinter as tk
 import csv
+from utils import BlankImg
+
 
 class Painting():
     def __init__(self, K, shape):
-        #self.radius = 5
+        #self.radius = 3
         self.K = K
         self.size = shape
         self.count = 0
         self.fixcount = 0
-        self.brush_size = 5
+        self.brush_size = 3
         self.img = np.zeros((self.size))
         for i in range(0, self.size[0]):
             for j in range(0, self.size[1]):
                 self.img[i, j, 0] = self.img[i, j, 1] = self.img[i, j, 2] = 255
 
-    def readfile_line(self, filename):
-        self.brush_size = 10
-        with open (filename, 'r', newline='') as csvfile:
-            rows = csv.reader(csvfile)
-            #count = 0
-            for row in rows:
-                if (len(row) == 6):
-                    #print (row)
-                    r, g, b = int(row[3]), int(row[4]), int(row[5])
-                    print (r, g, b)
-                
-                else:
-                    if (len(row) == 2):
-                        x = int(row[0])
-                        y = int(row[1])
-                        for i in range(x-10, x+10):
-                            if (i >= 0 and i < self.size[0]):
-                                self.img[i, y, 0] = r
-                                self.img[i, y, 1] = g
-                                self.img[i, y, 2] = b
+    def Painting(self):
+        img = BlankImg(self.size)
+        self.color_list = []
+        for i in range(0, self.K):
+            filename = "./points/" + str(i) + "_point.csv"
+            with open(filename, newline='') as csvfile:
+                rows = csv.reader(csvfile)
+                for row in rows:
+                    print(row)
+                    if (len(row) != 2):
+                        r, g, b = int(row[3]), int(row[4]), int(row[5])
+                        self.color_list.append((r, g, b))
                     else:
                         x = int(row[0])
-                        start_y = int(row[1])
-                        end_y = int(row[2])
-                        for i in range(x-self.brush_size, x+self.brush_size):
-                            if (i >= 0 and i < self.size[0]):
-                                for j in range(start_y, end_y):
-                                    self.img[i, j, 0] = r
-                                    self.img[i, j, 1] = g
-                                    self.img[i, j, 2] = b
-
-        save_name = "./painting/" + str(self.count) + "_paint.png"
-        cv.imwrite(save_name, self.img)
-        self.count += 1
-        print ("save name: ", save_name)
-        return (self.count)
-    
-    def readfile_points(self, filename):
-        self.brush_size = 3
-        with open (filename, 'r', newline='') as csvfile:
-            rows = csv.reader(csvfile)
-            #count = 0
-            for row in rows:
-                if (len(row) == 6):
-                    r, g, b = int(row[3]), int(row[4]), int(row[5])
-                    print (r, g, b)
-                
-                else:
-                    if (len(row) == 2):
-                        x = int(row[0])
                         y = int(row[1])
-                        for i in range(x-self.brush_size, x+self.brush_size):
-                            if (i >= 0 and i < self.size[0]):
-                                for j in range(y-self.brush_size, y+self.brush_size):
-                                    if (j >= 0 and j < self.size[1]):
-                                        self.img[i, j, 0] = r
-                                        self.img[i, j, 1] = g
-                                        self.img[i, j, 2] = b
-                    else:
-                        pass
+                        for a in range(x-self.brush_size, x+self.brush_size):
+                            for b in range(y-self.brush_size, y+self.brush_size):
+                                if (a >= 0 and a <= self.size[0]-1):
+                                    if (b >= 0 and b <= self.size[1]-1):
+                                        img[a, b, 0] = r
+                                        img[a ,b ,1] = g
+                                        img[a ,b, 2] = b
+                save_name = "./painting/" + str(i) + ".png"
+                cv.imwrite(save_name, img)
+                words = "finished " + str(i)
+                print (words)
+        return (self.color_list)
 
-        save_name = "./painting/" + str(self.count) + "_paint.png"
-        cv.imwrite(save_name, self.img)
+    def DectectImg(self, targetname, comparename):
+        target_img = cv.imread(targetname)
+        compare_img = cv.imread(comparename)
+        different_img = BlankImg(self.size)
+        for x in range(0, self.size[0]):
+            for y in range(0, self.size[1]):
+                if (int(target_img[x, y, 0]) != int(compare_img[x, y, 0])):
+                    different_img[x, y, 0] = target_img[x, y, 0]
+                    different_img[x, y, 1] = target_img[x, y, 1]
+                    different_img[x, y, 2] = target_img[x, y, 2]
+                else:
+                    if (int(target_img[x, y, 1]) != int(compare_img[x, y, 1])):
+                        different_img[x, y, 0] = target_img[x, y, 0]
+                        different_img[x, y, 1] = target_img[x, y, 1]
+                        different_img[x, y, 2] = target_img[x, y, 2]
+                    else:
+                        if (int(target_img[x, y, 2]) != int(compare_img[x, y, 2])):
+                            different_img[x, y, 0] = target_img[x, y, 0]
+                            different_img[x, y, 1] = target_img[x, y, 1]
+                            different_img[x, y, 2] = target_img[x, y, 2]
+        save_name = "./difference/" + str(self.count) + ".png"
+        cv.imwrite(save_name, different_img)
         self.count += 1
-        print ("save name: ", save_name)
-        return (self.count)
-    
+
+
+
+
+
+    """
     def DectectImg(self, targetname, comparedname):
         targetimg = cv.imread(targetname)
         comparedimg = cv.imread(comparedname)
@@ -112,14 +104,16 @@ class Painting():
         print ("save name: ", save_name)
         self.fixcount += 1
         return (save_name)
-
+    """
 
 if __name__ == "__main__":
-    K = 6
-    new = Painting(K, (399, 600, 3))
-    filename = "./points/0_line.csv"
-    new.readfile_line(filename)
-    for i in range(0, 5):
-        filename = "./points/" + str(i) + "_point.csv"
-        new.readfile_points(filename)
-    new.DectectImg("K_6_sunflower.png", "./painting/5_paint.png")
+    K = 298
+    filename = "K_298_1_2.png"
+    img = cv.imread(filename)
+    size = img.shape
+    new = Painting(K, size)
+    #filename = "./points/0_line.csv"
+    color_list = new.Painting()
+    comparename = "./painting/297.png"
+    new.DectectImg(filename, comparename)
+    print ("finished.")
